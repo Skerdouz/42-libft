81dea8768c758ffce3030b25ec0237a0e6fb5bee
Lucas Brahinski <lbrahins@paul-f4Br8s1.clusters.42paris.fr>
2024-05-16 17:26:03 +0200

    up

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,20 +6,25 @@
 /*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 16:33:17 by lbrahins          #+#    #+#             */
-/*   Updated: 2024/05/16 16:33:18 by lbrahins         ###   ########.fr       */
+/*   Updated: 2024/05/16 17:06:11 by lbrahins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	int		i;
+	size_t	i;
 	char	*pdest;
 	char	*psrc;
 
 	pdest = (char *)dest;
 	psrc = (char *)src;
+	i = 0;
 	while (i < n)
+	{
 		pdest[i] = psrc[i];
+		i++;
+	}
+	return (dest);
 }
--- wip/ft_memmove.c
+++ wip/ft_memmove.c
@@ -2,5 +2,13 @@
 
 void	*memmove(void *dest, const void *src, size_t n)
 {
-	
+	int		i;
+	char	*pdest;
+	char	*psrc;
+
+	pdest = (char *)dest;
+	psrc = (char *)src;
+	while (i < n)
+		pdest[i] = psrc[i++];
+	return (dest);
 }
