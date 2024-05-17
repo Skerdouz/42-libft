6efd3a14f339a5ab143f108896451091b22a41b0
Lucas Brahinski <lbrahins@paul-f4Br7s1.clusters.42paris.fr>
2024-05-17 12:22:20 +0200

    added ft_memchr

---
+++ ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 12:22:01 by lbrahins          #+#    #+#             */
+/*   Updated: 2024/05/17 12:22:02 by lbrahins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	unsigned char	*p;
+
+	p = (unsigned char *)s;
+	while (n-- > 0)
+	{
+		if (*p == c)
+			return ((void *)p);
+		p++;
+	}
+	return (NULL);
+}
