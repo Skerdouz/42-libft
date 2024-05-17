50ade2215f6ad1921f3abb112c66dc7a4aaadb35
Lucas Brahinski <lbrahins@bess-f4r2s11.clusters.42paris.fr>
2024-05-17 14:25:03 +0200

    updated ft_calloc

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/17 14:17:34 by lbrahins          #+#    #+#             */
+/*   Updated: 2024/05/17 14:23:49 by lbrahins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*memalloc;
+
+	memalloc = malloc(nmemb * size);
+	if (!memalloc)
+		return (NULL);
+	memalloc = ft_memset(memalloc, 0, nmemb);
+	return (memalloc);
+}
