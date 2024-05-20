53e2e76f8d7faf7978ddcbfbe7e70d929f9532e1
Lucas Brahinski <lbrahins@paul-f4Br3s1.clusters.42paris.fr>
2024-05-20 13:43:15 +0200

    update strjoin

---
+++ ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/20 13:41:26 by lbrahins          #+#    #+#             */
+/*   Updated: 2024/05/20 13:42:11 by lbrahins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	char	*str;
+
+	i = ft_strlen(s1) + ft_strlen(s2);
+	str = malloc(i * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (*s1)
+		str[i++] = *s1;
+	while (*s2)
+		str[i++] = *s2;
+	str[i] = '\0';
+	return (str);
+}
