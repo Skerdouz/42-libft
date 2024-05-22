d4e73ffef038c962d35718e39461452460db8077
Lucas Brahinski <lbrahins@paul-f4Br3s1.clusters.42paris.fr>
2024-05-22 16:41:11 +0200

    norm

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 10:37:14 by lbrahins          #+#    #+#             */
-/*   Updated: 2024/05/22 12:11:10 by lbrahins         ###   ########.fr       */
+/*   Updated: 2024/05/22 16:40:31 by lbrahins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,12 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		while (n-- > 0)
 			pdest[n] = psrc[n];
 	else
+	{
 		while (i < n)
 		{
 			pdest[i] = psrc[i];
 			i++;
 		}
+	}
 	return (dest);
 }
