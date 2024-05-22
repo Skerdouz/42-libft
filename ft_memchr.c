6c1d3d4c2f973205aac95901a5fbc778d61ee098
Lucas Brahinski <lbrahins@paul-f4Br3s1.clusters.42paris.fr>
2024-05-22 13:38:51 +0200

    unsigned char and black magic

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/17 12:22:01 by lbrahins          #+#    #+#             */
-/*   Updated: 2024/05/21 09:35:13 by lbrahins         ###   ########.fr       */
+/*   Updated: 2024/05/22 13:34:23 by lbrahins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	char	*p;
+	unsigned char	*p;
 
-	p = (char *)s;
+	p = (unsigned char *)s;
 	while (n-- > 0)
 	{
-		if (*p == c)
+		if (*p == (unsigned char)c)
 			return ((void *)p);
 		p++;
 	}
