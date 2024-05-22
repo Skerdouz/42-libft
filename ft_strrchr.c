b6c25fbf9e2b7035722bce4741bd082e073b6546
Lucas Brahinski <lbrahins@paul-f4Br3s1.clusters.42paris.fr>
2024-05-22 12:56:08 +0200

    it works

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 13:52:28 by lbrahins          #+#    #+#             */
-/*   Updated: 2024/05/22 10:50:15 by lbrahins         ###   ########.fr       */
+/*   Updated: 2024/05/22 12:55:12 by lbrahins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,15 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
-
+	char chr;
+	
+	chr = c;
 	i = ft_strlen(s);
-	while (i > 0)
-		if (s[i--] == c)
-			break ;
-	return ((char *)s);
+	while (i != -1)
+	{
+		if (s[i] == chr)
+			return ((char *)&s[i]);
+		i--;
+	}
+	return (NULL);
 }
