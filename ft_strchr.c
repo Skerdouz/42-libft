1b9fac65b730d288088d1673c2e7fa8cf4389041
Lucas Brahinski <lbrahins@paul-f4Br3s1.clusters.42paris.fr>
2024-05-22 13:04:52 +0200

    test: OK

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 13:18:01 by lbrahins          #+#    #+#             */
-/*   Updated: 2024/05/21 10:45:52 by lbrahins         ###   ########.fr       */
+/*   Updated: 2024/05/22 13:04:14 by lbrahins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,16 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	while (*s)
+	size_t	i;
+	char chr;
+
+	chr = c;
+	i = 0;
+	while (i <= ft_strlen(s))
 	{
-		if (*s == c)
-			return ((char *)s);
-		s++;
+		if (s[i] == chr)
+			return ((char *)&s[i]);
+		i++;
 	}
-	if (c == '\0')
-		return ((char *)s);
 	return (NULL);
 }
