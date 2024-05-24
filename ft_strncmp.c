d8745bef653c80afea583955fce0e36847098ce4
Lucas Brahinski <lbrahins@paul-f4Br3s1.clusters.42paris.fr>
2024-05-24 11:19:17 +0200

    fixed sussy return

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 10:59:35 by lbrahins          #+#    #+#             */
-/*   Updated: 2024/05/24 11:07:23 by lbrahins         ###   ########.fr       */
+/*   Updated: 2024/05/24 11:17:06 by lbrahins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	if (!n)
+	if (!n || s1 == s2)
 		return (0);
 	while (n-- > 0 && *s1 && *s2)
 	{
 		if (*s1 != *s2)
 			break ;
-		s1++;
-		s2++;
+		if (n > 0)
+		{
+			s1++;
+			s2++;
+		}
 	}
 	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
