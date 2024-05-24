b1cfba1124d3d27df0744737027b2fa8673ff64d
Lucas Brahinski <lbrahins@paul-f4Br3s1.clusters.42paris.fr>
2024-05-24 13:11:42 +0200

    working

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -1,15 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 09:50:53 by lbrahins          #+#    #+#             */
+/*   Updated: 2024/05/24 13:10:45 by lbrahins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*ptr;
+	t_list	*nextptr;
 
-	//TODO: do this function
+	if (!*lst)
+		return ;
 	ptr = *lst;
 	while (ptr->next)
 	{
-		ptr = ;
-		(*del)()
+		nextptr = ptr->next;
+		(*del)(ptr->content);
+		free(ptr);
+		ptr = nextptr;
 	}
-	
+	(*del)(ptr->content);
+	free(ptr);
+	*lst = NULL;
 }
