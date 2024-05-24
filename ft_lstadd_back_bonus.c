4428a9620ee205754a030fb0d645aa5ebb044eb3
Lucas Brahinski <lbrahins@paul-f4Br3s1.clusters.42paris.fr>
2024-05-24 11:59:11 +0200

    working

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -1,11 +1,23 @@
-#include "libft.h"
-#include <assert.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbrahins <lbrahins@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/24 09:52:12 by lbrahins          #+#    #+#             */
+/*   Updated: 2024/05/24 11:50:43 by lbrahins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "libft.h"
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*ptr;
-
-	ptr = ft_lstlast(*lst);
-	ptr->next = new;
+	if (!new)
+		return ;
+	if (!*lst)
+		*lst = new;
+	else
+		ft_lstlast(*lst)->next = new;
 }
